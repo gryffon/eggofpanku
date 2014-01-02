374ada6004ee7f5321b5b05340a4428351e326f3
gryffon <ryan@karetas.com>
2014-01-02 16:08:49 -0500

    Update location to install bitmap.

--- src/setup.py
+++ src/setup.py
@@ -63,7 +63,7 @@ SetCompressor lzma
 ;--------------------------------------------------------
 !define MUI_ABORTWARNING
 !define MUI_ICON "..\\..\\images\\icon.ico"
-!define MUI_WELCOMEFINISHPAGE_BITMAP "..\\installer_image.bmp"
+!define MUI_WELCOMEFINISHPAGE_BITMAP "..\\..\\install\\installer_image.bmp"
 
 !insertmacro MUI_PAGE_WELCOME
 !insertmacro MUI_PAGE_LICENSE "LICENSE"
