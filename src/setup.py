21086259964dc44ef6d1fc2f0420756679f720cb
gryffon <ryan@karetas.com>
2014-01-02 14:12:34 -0500

    Fix version in manifest.

--- src/setup.py
+++ src/setup.py
@@ -34,7 +34,7 @@ manifestxml = '''
 		<assemblyIdentity
 			type="win32"
 			name="Microsoft.VC90.CRT"
-			version="9.0.30729.4918"
+			version="9.0.21022.8"
 			processorArchitecture="X86"
 			publicKeyToken="1fc8b3b9a1e18e3b"
 			language="*"
