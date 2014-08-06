22a94e615a9829c55f3491233fc81d0d9b384070
gryffon <ryan@karetas.com>
2014-08-06 11:09:00 -0400

    Rework proxydb to use sqlalchemy

--- src/db/proxydb.py
+++ src/db/proxydb.py
@@ -26,7 +26,12 @@ file: proxydb.py
 date: 21 Jul 2014
 """
 
-import sqlite3
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 import hashlib
 import sys
 import os
@@ -41,141 +46,71 @@ from settings.xmlsettings import settings
 from settings.xmlsettings import locationsettings
 from settings import xmlfilters
 
+Base = declarative_base()
+
+class CardType(Base):
+	"""
+	Model of cardtypes table
+	"""
+	__tablename__ = 'cardtypes'
+
+	id = Column(Integer, primary_key=True)
+	type = Column(String)
+
+	cards = relationship("Card")
+
+	def __repr__(self):
+		return "<CardType (id='%s', type='%s'>" % (self.id, self.type)
+
+class Set(Base):
+	"""
+	Model of sets table
+	"""
+	__tablename__ = 'sets'
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String)
+
+	cards = relationship("Card")
+
+	def __repr__(self):
+		return "<Set (id='%s', name='%s'>" % (self.id, self.name)
+
+class Card(Base):
+	"""
+	Model of cards table
+	"""
+	__tablename__ = 'cards'
+
+	id = Column(String(10), primary_key=True)
+	name = Column(String)
+	type = Column(Integer, ForeignKey('cardtypes.id'))
+	set = Column(Integer, ForeignKey('sets.id'))
+	force = Column(Integer)
+	chi = Column(Integer)
+	hr = Column(Integer)
+	gc = Column(Integer)
+	ph = Column(Integer)
+	cardtext = Column(String)
+	image = Column(String)
+
+	def __repr__(self):
+		return "<Card (name='%s')>" % self.name
 
 class ProxyDB():
-	conn = None
+	"""
+	Controller for proxy database
+	"""
+	engine = None
+	session = None
 
 	def __init__(self):
 		#Open connection to database
-		self.conn = sqlite3.connect(os.path.join(locationsettings.data_dir, 'proxy.db'))
-		self.init_db()
+		self.engine = create_engine('sqlite:///proxy.db')
+		self.session = sessionmaker(bind=engine)
 
-	def init_db(self):
-		with self.conn:
-			cur = self.conn.cursor()
-			cur.execute("PRAGMA foreign_keys = ON")
 
-			#Debug
-			cur.execute("PRAGMA foreign_keys")
-			cur.execute('''CREATE TABLE IF NOT EXISTS CardType 
-							(Id Integer PRIMARY KEY,
-							 Type TEXT UNIQUE)
-						''')
-
-			cur.execute('''CREATE TABLE IF NOT EXISTS ProxyCard 
-							(Id TEXT PRIMARY KEY,
-							 Name TEXT UNIQUE,
-							 Type Integer,
-							 Force Integer DEFAULT 0,
-							 Chi Integer DEFAULT 0, 
-							 HR Integer DEFAULT 0,
-							 GC Integer DEFAULT 0,
-							 PH Integer DEFAULT 0,
-							 CardText Text,
-							 Image Text,
-							 FOREIGN KEY(Type) REFERENCES CardType(Id))
-						''')
-
-	def num_tables(self):
-		with self.conn:
-			cur = self.conn.cursor()
-			cur.execute("SELECT count(*) FROM sqlite_master WHERE type='table'")
-
-			numTables = cur.fetchone()[0]
-			print numTables
-
-	def reset_db(self):
-		with self.conn:
-			self.conn.execute("DROP TABLE ProxyCard")
-
-	def add_card_type(self, type):
-		with self.conn:
-			try:
-				cur = self.conn.cursor()
-				cur.execute("INSERT INTO CardType VALUES (NULL, ?)", (type,))
-			except sqlite3.IntegrityError:
-				print "Card Type already exists."
-
-	def get_card_types(self):
-		with self.conn:
-			cur = self.conn.cursor()
-			cur.execute("SELECT * from CardType")
-
-			rows = cur.fetchall()
-
-			return rows
-
-	def add_card(self, carddata):
-		md5 = hashlib.md5()
-		md5.update(carddata['Name'])
-		IdHash = md5.hexdigest()
-		with self.conn:
-			try:
-				cur = self.conn.cursor()
-				cur.execute("INSERT INTO ProxyCard VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-							(IdHash, carddata['Name'], carddata['Type'], carddata['Force'],
-								carddata['Chi'], carddata['HR'], carddata['GC'], carddata['PH'],
-								carddata['CardText'], carddata['Image']))
-			except sqlite3.IntegrityError:
-				error = sys.exc_info()[1]
-				if str(error) == "foreign key constraint failed":
-					print "Card Type does not exist."
-				elif str(error) == "column Name is not unique":
-					print "Proxy Card with that name already exists."
-				else:
-					print "Unknown issue adding Proxy Card."
-
-	def del_card(self, Id):
-		with self.conn:
-			cur = self.conn.cursor()
-			cur.execute("DELETE FROM ProxyCard WHERE Id=?", (Id,))
-
-	def get_card_by_id(self, Id):
-		with self.conn:
-			cur = self.conn.cursor()
-			cur.execute("SELECT * FROM ProxyCard WHERE Id=?", (Id,))
-
-			row = cur.fetchone()
-
-			return row
-
-	def get_cards_by_type(self, Type):
-		with self.conn:
-			cur = self.conn.cursor()
-			cur.execute("SELECT * FROM ProxyCard WHERE Type=?", (Type,))
-
-			rows = cur.fetchall()
-
-			return rows
 
 #Use for testing
 if __name__ == "__main__":
 	proxydb = ProxyDB()
-	proxydb.num_tables()
-#	proxydb.reset_db()
-
-#	proxydb.add_card_type("Personality")
-#	proxydb.add_card_type("Item")
-	rows = proxydb.get_card_types()
-
-	for row in rows:
-		print row
-'''
-	newcard = {'Name': 'Fudo', 'Type': 4, 'Force': 10, 'Chi': 5, 'HR': -1, 'GC': 0, 'PH': 0,
-				'CardText': 'May not be included in decks.\nCards\' effects will not bow this Personality or move him home.', 
-				'Image': '' }
-	proxydb.add_card(newcard)
-	md5 = hashlib.md5()
-	md5.update('Fudo')
-	CardId = md5.hexdigest()
-
-	cards = proxydb.get_cards_by_type(1)
-	for card in cards:
-		print card
-
-	proxydb.del_card(CardId)
-
-	cards = proxydb.get_cards_by_type(1)
-	for card in cards:
-		print card
-'''
