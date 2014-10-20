dc2850c768f1f67e919d1c4b55f0cd78ce7fc9d7
gryffon <ryan@karetas.com>
2014-10-20 15:52:08 -0400

    Continue backing out proxydb changes.

--- src/eopk.py
+++ src/eopk.py
@@ -36,7 +36,6 @@ from gui import canvas, playfield, dragdrop, preview
 from gui import settings_ui, deckpanel, deckedit
 from game import game, deck
 from net import netcore
-from gui import proxygui
 
 
 
@@ -53,6 +52,94 @@ LinkClickEvent, EVT_LINK_CLICK = wx.lib.newevent.NewEvent()
 CreatedCards = []  # A mapping helper for the 'Create card' menu.
 
 
+class CreateCardDialog(wx.Dialog):
+	cardTypes = {
+		'Personality':'personality',
+		'Follower':'follower',
+		'Item':'item',
+		'Region':'region',
+		'Holding':'holding',
+	}
+	
+	def GetType(self):
+		return self.cardTypes[self.cmbType.GetValue()]
+	
+	def GetStats(self):
+		return {
+			'type':self.GetType(),
+			'name':self.txtName.GetValue(),
+			'name':self.txtName.GetValue(),
+			'force':self.txtForce.GetValue(),
+			'chi':self.txtChi.GetValue(),
+			'honor_req':self.txtHonor.GetValue(),
+			'cost':self.txtCost.GetValue(),
+			'personal_honor':self.txtPH.GetValue(),
+			'text':self.txtText.GetValue(),
+		}
+		
+	def __init__(self, parent):
+		wx.Dialog.__init__(self, parent, wx.ID_ANY, 'Create Card')
+		sizer = wx.BoxSizer(wx.VERTICAL)
+		
+		
+		sbsizer = wx.StaticBoxSizer(wx.StaticBox(self, -1, 'Card Type'), wx.VERTICAL)
+		
+		self.cmbType = wx.ComboBox(self, size=(200,-1), style=wx.CB_READONLY)
+		for type in self.cardTypes:
+			self.cmbType.Append(type)
+		self.cmbType.SetValue('Personality')
+		sbsizer.Add(self.cmbType, 0, wx.CENTRE|wx.ALL|wx.EXPAND, 5)
+		
+		sizer.Add(sbsizer, 0, wx.EXPAND | wx.ALL, 5)
+
+
+		sbsizer = wx.StaticBoxSizer(wx.StaticBox(self, -1, 'Card Data'), wx.VERTICAL)
+		
+		sbsizer.Add(wx.StaticText(self, label='Name:'), 0, wx.ALL, 5)
+		self.txtName = wx.TextCtrl(self)
+		sbsizer.Add(self.txtName, 0, wx.CENTRE|wx.ALL|wx.EXPAND, 5)
+		
+		sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+		self.txtForce = wx.TextCtrl(self, size=(48, -1))
+		sizer2.Add(wx.StaticText(self, label='Force:'), 0, wx.CENTRE|wx.ALL, 5)
+		sizer2.Add(self.txtForce, 0, wx.CENTRE|wx.ALL, 5)
+		self.txtChi = wx.TextCtrl(self, size=(48, -1))
+		sizer2.Add(wx.StaticText(self, label='Chi:'), 0, wx.CENTRE|wx.ALL, 5)
+		sizer2.Add(self.txtChi, 0, wx.CENTRE|wx.ALL, 5)
+		sbsizer.Add(sizer2, 0, wx.CENTRE|wx.ALL|wx.EXPAND, 0)
+
+		sizer2 = wx.BoxSizer(wx.HORIZONTAL)
+		self.txtHonor = wx.TextCtrl(self, size=(48, -1))
+		sizer2.Add(wx.StaticText(self, label='HR:'), 0, wx.CENTRE|wx.ALL, 5)
+		sizer2.Add(self.txtHonor, 0, wx.CENTRE|wx.ALL, 5)
+		self.txtCost = wx.TextCtrl(self, size=(48, -1))
+		sizer2.Add(wx.StaticText(self, label='GC:'), 0, wx.CENTRE|wx.ALL, 5)
+		sizer2.Add(self.txtCost, 0, wx.CENTRE|wx.ALL, 5)
+		self.txtPH = wx.TextCtrl(self, size=(48, -1))
+		sizer2.Add(wx.StaticText(self, label='PH:'), 0, wx.CENTRE|wx.ALL, 5)
+		sizer2.Add(self.txtPH, 0, wx.CENTRE|wx.ALL, 5)
+		sbsizer.Add(sizer2, 0, wx.CENTRE|wx.ALL|wx.EXPAND, 0)
+		
+		sbsizer.Add(wx.StaticText(self, label='Card text:'), 0, wx.ALL, 5)
+		self.txtText = wx.TextCtrl(self, size=(-1, 100), style=wx.TE_MULTILINE)
+		sbsizer.Add(self.txtText, 0, wx.CENTRE|wx.ALL|wx.EXPAND, 5)
+
+		sizer.Add(sbsizer, 0, wx.EXPAND | wx.ALL, 5)
+
+		
+		buttonsizer = wx.StdDialogButtonSizer()
+		self.btnSubmit = wx.Button(self, wx.ID_OK, 'Create')
+		self.btnSubmit.SetDefault()
+		self.btnCancel = wx.Button(self, wx.ID_CANCEL)
+		buttonsizer.Add(self.btnSubmit, 1, wx.EXPAND|wx.ALL, 5)
+		buttonsizer.Add(self.btnCancel, 1, wx.EXPAND|wx.ALL, 5)
+		buttonsizer.Realize()
+		
+		sizer.Add(buttonsizer, 0, wx.EXPAND | wx.ALL, 0)
+		self.SetSizer(sizer)
+		sizer.Fit(self)
+	
+	
 class ConnectDialog(wx.Dialog):
 	def __init__(self, parent):
 		wx.Dialog.__init__(self, parent, wx.ID_ANY, 'Connect to host')
@@ -1385,7 +1472,7 @@ class MainWindow(wx.Frame):
 	
 	def OnMenuCreateCard(self, event):
 		if self.client and self.client.Playing():
-			dlg = proxygui.CreateCardDialog(self)
+			dlg = CreateCardDialog(self)
 			if dlg.ShowModal() == wx.ID_OK:
 				self.client.Send(netcore.Msg('create-card', **dlg.GetStats()))
 	
